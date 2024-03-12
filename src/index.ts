7fa1f6a76614f43d953a4577ccff7d86c07a8586
Anay <anayparswani2008@gmail.com>
2024-03-12 12:16:07 +0530

    improve code

--- src/index.ts
+++ src/index.ts
@@ -21,18 +21,16 @@ interface Options {
 }
 
 interface FindOptions {
-  sort?: object,
-  limit?: number
-  skip?: number
+  sort?: object;
+  limit?: number;
+  skip?: number;
 }
-
 
 interface InsertOne {
   insertedId: string;
-
 }
 
-interface insertMany {
+interface InsertMany {
   insertedIds: string[];
 }
 
@@ -42,7 +40,7 @@ interface Update {
 }
 
 interface Delete {
-  deletedCount: 1
+  deletedCount: number;
 }
 
 class MongoApi<T = object | object[]> {
@@ -62,12 +60,12 @@ class MongoApi<T = object | object[]> {
       API_KEY,
       dataSource,
     }: { url: string; API_KEY: string; dataSource: string },
-    requestOptions: object  = {}
+    requestOptions: object = {}
   ) {
     if (!url || !API_KEY || !dataSource)
       throw new Error("Missing required parameters.");
     if (!url.endsWith("/")) url += "/";
-    if(!url.endsWith("action/")) url += "action/";
+    if (!url.endsWith("action/")) url += "action/";
     this.API_KEY = API_KEY;
     this.url = url;
     this.dataSource = dataSource;
@@ -94,155 +92,159 @@ class MongoApi<T = object | object[]> {
       },
     });
     const json = (await response.json()) as { documents?: T[] };
-    return json.documents ? json.documents : json;
+    return json.documents || json;
   }
-db(databaseName: string) {
-  const newInstance = new MongoApi({
-    url: this.url,
-    API_KEY: this.API_KEY,
-    dataSource: this.dataSource
-  }, this.requestOptions || {});
-  newInstance.databaseName = databaseName;
-  return newInstance;
-}
+  db(databaseName: string) {
+    const newInstance = new MongoApi(
+      {
+        url: this.url,
+        API_KEY: this.API_KEY,
+        dataSource: this.dataSource,
+      },
+      this.requestOptions || {}
+    );
+    newInstance.databaseName = databaseName;
+    return newInstance;
+  }
 
-collection<Schema = T>(collectionName: string) {
-  const newInstance = new MongoApi({
-    url: this.url,
-    API_KEY: this.API_KEY,
-    dataSource: this.dataSource
-  }, this.requestOptions || {});
-  newInstance.databaseName = this.databaseName;
-  newInstance.collectionName = collectionName;
-  return newInstance as unknown as MongoApi<Schema>;
-}
-/**
- * Find documents in the collection.
- * @param {object} filter - The filter criteria.
- * @param {object} projection - The projection criteria.
- * @param {FindOptions} options - The options for sorting and limiting the results.
- * @returns {Promise<T>} The found documents.
- */
-async find(
-  filter: object = {},
-  projection: object = {},
-  options: FindOptions = {}
-): Promise<T[]> {
-  this.action = "find";
-  this.options = {
-    method: "POST",
-    body: {
-      filter,
-      projection,
-      ...options,
-    },
-  };
-  return await this.dispatchRequest(this.action, this.options) as T[];
-}
+  collection<Schema = T>(collectionName: string) {
+    const newInstance = new MongoApi(
+      {
+        url: this.url,
+        API_KEY: this.API_KEY,
+        dataSource: this.dataSource,
+      },
+      this.requestOptions || {}
+    );
+    newInstance.databaseName = this.databaseName;
+    newInstance.collectionName = collectionName;
+    return newInstance as unknown as MongoApi<Schema>;
+  }
+  /**
+   * Find documents in the collection.
+   * @param {object} filter - The filter criteria.
+   * @param {object} projection - The projection criteria.
+   * @param {FindOptions} options - The options for sorting and limiting the results.
+   * @returns {Promise<T[]>} The found documents.
+   */
+  find(
+    filter: object = {},
+    projection: object = {},
+    options: FindOptions = {}
+  ): Promise<T[] | []> {
+    const action =  "find";
+    const reqOptions = {
+      method: "POST",
+      body: {
+        filter,
+        projection,
+        ...options,
+      },
+    };
+    return this.dispatchRequest(action, reqOptions) as Promise<T[]>;
+  }
 
   /**
    * Perform an aggregation pipeline operation.
    * @param {object[]} pipeline - The aggregation pipeline.
    * @returns {Promise<T>} The result of the operation.
    */
-  async aggregate(pipeline: object[]) {
-    this.action = "aggregate";
-    this.options = {
+  aggregate(pipeline: object[]) {
+    const action =  "aggregate";
+    const reqOptions = {
       method: "POST",
       body: {
         pipeline,
       },
     };
-    return await this.dispatchRequest(this.action, this.options) as any; //TODO
+    return  this.dispatchRequest(action, reqOptions) as Promise<T>; //TODO
   }
 
-
-
-
-/**
- * Insert one document into the collection.
- * @param {object} document - The document to insert.
- * @returns {Promise<InsertOne>} The result of the operation.
- */
-async insertOne(document: object) {
-  this.action = "insertOne";
-  this.options = {
-    method: "POST",
-    body: {
-      document,
-    },
-  };
-  return await this.dispatchRequest(this.action, this.options) as InsertOne;
-}
-
-/**
- * Insert many documents into the collection.
- * @param {object[]} documents - The documents to insert.
- * @returns {Promise<object>} The result of the operation.
- */
-async insertMany(documents: object[]) {
-  this.action = "insertMany";
-  this.options = {
-    method: "POST",
-    body: {
-      documents,
-    },
-  };
-  return await this.dispatchRequest(this.action, this.options) as insertMany;
-}
-
-/**
- * Update one document in the collection.
- * @param {object} filter - The filter criteria.
- * @param {object} update - The update operations to be applied to the document.
- * @returns {Promise<object>} The result of the operation.
- */
-async updateOne(filter: object, update: object) {
-  this.action = "updateOne";
-  this.options = {
-    method: "POST",
-    body: {
-      filter,
-      update,
-    },
-  };
-  return await this.dispatchRequest(this.action, this.options) as Update;
-}
-
+  /**
+   * Insert one document into the collection.
+   * @param {object} document - The document to insert.
+   * @returns {Promise<InsertOne>} The result of the operation.
+   */
+  insertOne(document: object) {
+    const action =  "insertOne";
+    const reqOptions = {
+      method: "POST",
+      body: {
+        document,
+      },
+    };
+    return this.dispatchRequest(action, reqOptions) as Promise<InsertOne>;
+  }
 
   /**
-   * Update many documents in the collection.
-   * @param {object} filter - The filter criteria.
-   * @param {object} update - The update operations to be applied to the documents.
-   * @returns {Promise<object>} The result of the operation.
+   * Insert many documents into the collection.
+   * @param {object[]} documents - The documents to insert.
+   * @returns {Promise<InsertMany>} The result of the operation.
    */
-  async updateMany(filter: object, update: object) {
-    this.action = "updateMany";
-    this.options = {
+  insertMany(documents: object[]) {
+    const action =  "insertMany";
+    const reqOptions = {
+      method: "POST",
+      body: {
+        documents,
+      },
+    };
+    return  this.dispatchRequest(
+      action,
+      reqOptions
+    ) as Promise<InsertMany>;
+  }
+
+  /**
+   * Update one document in the collection.
+   * @param {object} filter - The filter criteria.
+   * @param {object} update - The update operations to be applied to the document.
+   * @returns {Promise<Update>} The result of the operation.
+   */
+   updateOne(filter: object, update: object) {
+    const action =  "updateOne";
+    const reqOptions = {
       method: "POST",
       body: {
         filter,
         update,
       },
     };
-    return await this.dispatchRequest(this.action, this.options) as Update;
+    return  this.dispatchRequest(action, reqOptions) as Promise<Update>;
   }
 
+  /**
+   * Update many documents in the collection.
+   * @param {object} filter - The filter criteria.
+   * @param {object} update - The update operations to be applied to the documents.
+   * @returns {Promise<Update>} The result of the operation.
+   */
+  updateMany(filter: object, update: object) {
+    const action =  "updateMany";
+    const reqOptions = {
+      method: "POST",
+      body: {
+        filter,
+        update,
+      },
+    };
+    return this.dispatchRequest(action, reqOptions) as Promise<Update>;
+  }
 
   /**
    * Delete one document from the collection.
    * @param {object} filter - The filter criteria.
-   * @returns {Promise<object>} The result of the operation.
+   * @returns {Promise<Delete>} The result of the operation.
    */
-  async deleteOne(filter: object) {
-    this.action = "deleteOne";
-    this.options = {
+   deleteOne(filter: object) {
+    const action =  "deleteOne";
+    const reqOptions = {
       method: "POST",
       body: {
         filter,
       },
     };
-    return await this.dispatchRequest(this.action, this.options) as Delete;
+    return  this.dispatchRequest(action, reqOptions) as Promise<Delete>;
   }
 
   /**
@@ -250,19 +252,16 @@ async updateOne(filter: object, update: object) {
    * @param {object} filter - The filter criteria.
    * @returns {Promise<T>} The result of the operation.
    */
-  async deleteMany(filter: object) {
-    this.action = "deleteMany";
-    this.options = {
+  deleteMany(filter: object) {
+    const action =  "deleteMany";
+    const reqOptions = {
       method: "POST",
       body: {
         filter,
       },
     };
-    return await this.dispatchRequest(this.action, this.options) as Delete;
+    return this.dispatchRequest(action, reqOptions) as Promise<Delete>;
   }
-
-
-
 }
 
 export default MongoApi;
